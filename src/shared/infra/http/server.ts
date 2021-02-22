d886623a0fc8b7fd479f1f2e6c11def699673d4f
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-22 10:18:23 -0300

    test: testing CD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(routes);
 
 app.get('/teste', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World Teste de CD3' });
+  res.json({ message: 'Hello World Teste de CD4' });
 });
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
